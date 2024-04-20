2ec638a31140ca7b0ea043cc57a895dfe9e08857
calithos4136 <calithos4136@gmail.com>
2024-04-20 18:27:45 -0500

    added placeholder text

--- .venv/source/lib/main.py
+++ .venv/source/lib/main.py
@@ -9,11 +9,8 @@ def run():
     setup.run()
 
     if len(sys.argv) < 2:
-        yes = IO.say(["what is your name", "what is your dob?", "how are you feeling today"], True, True, True, "internal")
-        IO.say(yes, isLoop=True)
+       print()
 
     else:
-
-        yes = IO.say("what is your fav color ", True, syntaxChk=True, synType="internal")
-        IO.say(yes)
+        print()
 
