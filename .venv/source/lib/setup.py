b07f6fbd85c520e9fc5fc3815439261d5bb2228f
calithos4136 <calithos4136@gmail.com>
2024-04-20 20:03:43 -0500

    added files to the setup file in setup.py

--- .venv/source/lib/setup.py
+++ .venv/source/lib/setup.py
@@ -1,10 +1,16 @@
 import platform
-
+import os
+import sys
+from . import web
 
 # a file to initialize the files and services needed to run the program
 
 def run():
     fileSetup()
+
+    if not isOnline():
+        sys.exit()
+
 
 
 def fileSetup():
@@ -16,3 +22,29 @@ def fileSetup():
         slash = "\\"
     else:
         slash = "/"
+
+    homePath = os.path.expanduser("~") + slash
+    sysPath = os.getcwd()
+    configPath = sysPath + slash + "config" + slash
+
+    configFile = configPath + "config.yaml"
+
+    if os.path.isdir(configPath):
+
+        if not os.path.isfile(configFile):
+            print("hello")
+
+    else:
+        if os.mkdir(configPath) is not None:
+            print("Failed to make configuration directory.")
+            sys.exit()
+        else:
+            print("hello")
+
+# check for internet condition
+def isOnline():
+    if not web.ping():
+        print("No internet discovered. Please establish an internet connection before running this program.")
+        return False
+    else:
+        return True
