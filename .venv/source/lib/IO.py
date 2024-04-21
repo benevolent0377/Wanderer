af1a26ab59dfda7ccb0504a15c2405565a2d49bf
calithos4136 <calithos4136@gmail.com>
2024-04-20 22:50:13 -0500

    made significant changes

--- .venv/source/lib/IO.py
+++ .venv/source/lib/IO.py
@@ -2,6 +2,8 @@
 from . import syntax
 import yaml
 
+
+
 # simply prints the string output or creates a query in the terminal, also returns the inputted values
 def say(output, isQuestion=False, isLoop=False, syntaxChk=False, synType="", end="\n", ):
     # checking if the program wants a response, aka: question = yes
