857d12309c879c47273f4195313c77bc849f1f7d
calithos4136 <calithos4136@gmail.com>
2024-04-21 19:13:43 -0500

    added logging capabilities

--- .venv/source/lib/web.py
+++ .venv/source/lib/web.py
@@ -1,7 +1,8 @@
 import socket
 import ftplib
 from . import IO
-from . import setup
+from . import log
+from . import system
 # a file for all the HTTP and HTTPS functions
 hostName = "calithos.in"
 usr = "wanderer@calithos.in"
@@ -18,29 +19,44 @@ def ping(host="8.8.8.8", port=53, timeout=3):
         socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
         return True
     except socket.error as ex:
-        print(ex)
+        log.log(ex, "err")
         return False
 
 def fetchFTP(localFile, remoteFile, timeout=2000):
     with ftplib.FTP(hostName, timeout=timeout) as ftp:
         ftp.login(user=usr, passwd=pwd)
+        log.log(hostName, "ftpconnect", usr)
         ftp.encoding = "utf-8"
         with open(localFile, "wb") as file:
             ftp.retrbinary(f"RETR {remoteFile}", file.write)
+            log.log(remoteFile, "ftpD", hostName)
 
 
-def postFTP(localFile, remoteFile, timeout=2000, isTmp=False):
+def postFTP(localFile, remoteFile, timeout=2000, isTmp=False, isYaml=False, keyword=""):
     with ftplib.FTP(hostName, timeout=timeout) as ftp:
         ftp.login(user=usr, passwd=pwd)
+        log.log(hostName, "ftpconnect", usr)
         ftp.encoding = "utf-8"
         if isTmp:
-            with open(localFile, "xw") as file:
-                file.write(IO.yamlRead(f"{setup.getConfigPath()}{setup.getSlash()}local.yaml", "Program-SerialNo"))
 
-                ftp.storbinary(f"STOR {remoteFile}", file)
+            with open(localFile, "xw") as file:
+                if isYaml:
+                    data = IO.yamlRead(localFile, keyword)
+                    file.write(data)
+                else:
+                    file = IO.fileRead(localFile)
+
+                ftp.storlines(f"STOR {remoteFile}", file)
+                log.log(remoteFile, "ftpD", hostName)
 
         else:
-            with open(localFile, "xw") as file:
-                file.write(IO.yamlRead(f"{setup.getConfigPath()}{setup.getSlash()}local.yaml", "Program-SerialNo"))
 
-                ftp.storbinary(f"STOR {remoteFile}", file)
+            with open(localFile, "xw") as file:
+                if isYaml:
+                    data = IO.yamlRead(localFile, keyword)
+                    file.write(data)
+                else:
+                    file = IO.fileRead(localFile)
+
+                ftp.storlines(f"STOR {remoteFile}", file)
+                log.log(remoteFile, "ftpD", hostName)
