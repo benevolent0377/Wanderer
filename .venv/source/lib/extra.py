daec41f381baa0f3a04a75afaf04b41fce083368
calithos4136 <calithos4136@gmail.com>
2024-04-21 19:13:54 -0500

    added logging capabilities

--- .venv/source/lib/extra.py
+++ .venv/source/lib/extra.py
@@ -1,8 +1,10 @@
 import random
 import string
+from . import log
 # a file for misc commands
 def keyGen(length=16, seed=hash(random)):
 
     seed = seed.__str__()
     key = ''.join(random.choices(string.ascii_uppercase + string.digits + seed, k=length))
+    log.log("", "rand")
     return key
